454ec8f7043a53f1e41900f43d9f4f9ead3329fa
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-01-28 11:49:14 -0500

    begin calculations for meng recreation

---
+++ meng_recreation_calculcations.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# extracted CCES data
+cces_data = pd.read_csv("cces2024_meng_replication_set.csv")
+
+# results of the election
+full_results = pd.read_csv("presidential_vote_results.csv")
