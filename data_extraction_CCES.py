278de1f850504f560ad575ea5bd408c81218d56d
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-01-27 23:06:56 -0500

    updates to extracted set, new origin set

--- data_extraction_CCES.py
+++ data_extraction_CCES.py
@@ -5,41 +5,50 @@ import numpy as np
 # DATAFILE AVAILABLE FROM https://dataverse.harvard.edu/file.xhtml?fileId=13398263&version=2.0
 
 # load CCES 2024 data
-df = pd.read_csv("merged_recontact_2024_vv.csv")
+df = pd.read_csv("CCES24_Common_OUTPUT_vv_topost_final.csv")
 
 # these columns are the only ones needed to recreate the analysis
 neededcols = [
-    "inputstate_24",
+    "inputstate",
     "CC24_410",
     "TS_g2024",
-    "TS_voterstatus_24",
-    "commonweight_24"
+    "TS_voterstatus"
 ]
 
 df = df[neededcols].copy()
 
 # rename variables for ease of use
-df["state"] = df["inputstate_24"]
+df["state"] = df["inputstate"]
 
 # create binary vote indicators for trump and harris from vote choices offered
 df["X_trump"] = np.where(df["CC24_410"] == "Donald Trump", 1, 0)
 df["X_harris"]   = np.where(df["CC24_410"] == "Kamala Harris", 1, 0)
 
 # for those without a vote choice was not clear give an NA result
-df.loc[df["CC24_410"].isin(["Not sure", "N"]), ["X_trump", "X_harris"]] = np.nan
+df.loc[df["CC24_410"].isin(["Did not vote for President", "N"]), ["X_trump", "X_harris"]] = np.nan
 
-# indicator of self repoted voting is if they said they voted for a condaidate, do not count those reporting "not sure" or "N" as having voted
+# indicator of self repoted voting is if they said they voted for a candidate, do not count those reporting "Did not vote for President" or "N" as having voted
+candidate_list = [
+    "Kamala Harris",
+    "Donald Trump",
+    "Robert F. Kennedy Jr.",
+    "Jill Stein",
+    "Cornel West",
+    "Chase Oliver",
+    "Other"
+]
+
 df["selfvoted_2024"] = np.where(
-    df["CC24_410"].isin([
-        "Kamala Harris",
-        "Donald Trump",
-        "Robert F. Kennedy Jr.",
-        "Jill Stein",
-        "Cornel West",
-        "Chase Oliver",
-        "Other"
-    ]),
-    1, 0
+    df["CC24_410"].isna(),
+    np.nan,
+    np.where(df["CC24_410"].isin(candidate_list), 1, 0)
+)
+
+# handle voter registration match (only active or empty, so we don't know about those people)
+df["active_reg_24"] = np.where(
+    df["TS_voterstatus"] == 1,
+    1,
+    np.nan
 )
 
 # indicator for a validated voter
@@ -48,20 +57,17 @@ df["validated_voter"] = np.where(
     1, 0
 )
 
-# -----------------------------
-# 8. Final dataset
-# -----------------------------
+# create recreation dataset
 output_df = df[
     [
         "state",
         "X_trump",
         "X_harris",
         "selfvoted_2024",
-        "validated_voter",
-        "commonweight_24"
+        "validated_voter"
     ]
 ].copy()
 
 output_df.to_csv("cces2024_meng_replication_set.csv", index=False)
 
-print("Dataset created:", output_df.shape)
+print(output_df.shape)
