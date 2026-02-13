ec1b55326b70f490ab3e7f0a35d33c72afccb0f2
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-02-13 16:28:45 -0500

    initial notes from dataset

--- fiftyplusone_initial_analysis.py
+++ fiftyplusone_initial_analysis.py
@@ -5,3 +5,12 @@ import numpy as np
 
 # load CCES 2024 data from site
 df = pd.read_csv("data/president_2024_general.csv")
+
+######## Jacqui's Notes on How to Know What Is a Useful Question 
+
+# each poll has its own poll_id
+# each row is one answer in one question in one poll
+# each question has a question_id
+# the answers to a question can be accessed through a shared question_id x poll_id
+# the answers are in answer
+
