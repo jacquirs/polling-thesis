9561273635ef6984733311840f9186e7f3da57f6
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-02-13 16:16:41 -0500

    create intiial analysis doc for 50+1 data

---
+++ fiftyplusone_initial_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+# THIS FILE ANALYZES 50+1 data provided generously for 2024 polling
+
+# load CCES 2024 data from site
+df = pd.read_csv("data/president_2024_general.csv")
