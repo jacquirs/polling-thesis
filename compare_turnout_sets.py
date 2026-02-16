7502c466e7fbae3946f44dda5f4fa9f045fab091
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-02-16 12:22:23 -0500

    reclean without dropping votes

---
+++ compare_turnout_sets.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+########################################################################################
+############################ Reclean without dropping votes ############################
+########################################################################################
+# load truth set from site
+true_votes_preclean = pd.read_csv("data/2024-president-state.csv")
+
+# sort for 2024 presidential results
+df_pres_2024 = true_votes_preclean[
+    (true_votes_preclean["year"] == 2024) &
+    (true_votes_preclean["office"] == "US PRESIDENT")
+].copy()
+
+# did not drop  "UNDERVOTES", "OVERVOTES", "VOID", "NONE OF THESE CANDIDATES"
+
+# state names to match CCES
+df_pres_2024["state_name"] = df_pres_2024["state"].str.title()
+
+# full data for later use
+df_pres_2024.to_csv("presidential_vote_results.csv", index=False)
+
+# aggregate total votes per state
+state_totals = (
+    df_pres_2024
+    .groupby("state_name", as_index=False)["votes"]
+    .sum()
+    .rename(columns={"votes": "N_state"})
+)
+
+# aggregate Trump votes per state
+trump_votes = (
+    df_pres_2024[df_pres_2024["candidate"] == "TRUMP, DONALD J."]
+    .groupby("state_name")["votes"]
+    .sum()
+)
+
+# aggregate Harris votes per state
+harris_votes = (
+    df_pres_2024[df_pres_2024["candidate"] == "HARRIS, KAMALA D."]
+    .groupby("state_name")["votes"]
+    .sum()
+)
+
+state_totals["trump_votes"] = state_totals["state_name"].map(trump_votes)
+state_totals["harris_votes"] = state_totals["state_name"].map(harris_votes)
+
+
+# compute Trump share
+state_totals["p_trump_true"] = (
+    state_totals["trump_votes"] / state_totals["N_state"]
+)
+
+# compute harris share
+state_totals["p_harris_true"] = (
+    state_totals["harris_votes"] / state_totals["N_state"]
+)
+
+# keep only needed columns
+true_votes = state_totals[
+    ["state_name", "p_trump_true", "p_harris_true", "N_state"]
+].copy()
+
+
+
+# load turnout data from census
+census_turnout = pd.read_csv("data/turnout_by_state.csv")
+
+# calculate national row for votes by summing all states
+national_row = pd.DataFrame([{
+    "state_name": "national",
+    "N_state": true_votes["N_state"].sum()
+}])
+true_votes = pd.concat([true_votes, national_row], ignore_index=True)
+
+# merge and compare
+df = census_turnout.merge(true_votes[["state_name", "N_state"]], left_on="state", right_on="state_name", how="left")
+df = df.drop(columns="state_name")
+
+df["diff"] = df["N_state"] - df["total_voted"]
+df["diff_pct"] = (df["diff"] / df["total_voted"] * 100).round(2)
+
+print(df[["state", "total_voted", "N_state", "diff", "diff_pct"]].to_string(index=False))
