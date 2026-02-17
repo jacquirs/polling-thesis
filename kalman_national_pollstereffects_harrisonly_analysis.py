8d9e717ccd566624832cebcce60241db9848234e
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-02-16 22:37:50 -0500

    new file for pollster effects

---
+++ kalman_national_pollstereffects_harrisonly_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import warnings
+import sys
+from datetime import datetime
+warnings.filterwarnings('ignore')
+
+# THIS FILE ANALYZES POLLS USING KALMAN FILTERING/SMOOTHING
+# national level
+# harris v trump only
+# includes pollster level effects
