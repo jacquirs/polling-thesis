09fc43a2403cfb6a2352004a1ceb81da68afcc28
Jacqui Schlesinger <jacquirschlesinger@gmail.com>
2026-03-01 18:18:35 -0500

    comments to describe file

--- fiftyplusone_datelimited_simple_mode_analysis.py
+++ fiftyplusone_datelimited_simple_mode_analysis.py
@@ -4,6 +4,14 @@ import sys
 import statsmodels.api as sm
 from scipy import stats
 
+
+# THIS FILE ANALYZES POLL ACCURACY AND MODE EFFECTS FOR HARRIS+TRUMP POLLS USING THE SIMPLE MODES OUTPUT IN fiftyplusone_datelimited_analysis
+# IT IS LIMITED TO THE TIME AFTER BIDEN DROPPED OUT
+# using Martin, Traugott & Kennedy (2005) Method A accuracy measure
+# then runs multivariate ols regressions (state and national separately)
+# to understand what poll design factors predict accuracy
+
+
 # redirect all print output to a log file
 log_file = open('output/fiftyplusone_datelimited_simple_mode_analysis_log.txt', 'w')
 sys.stdout = log_file
